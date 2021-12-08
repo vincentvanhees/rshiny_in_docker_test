1c7fa72177293f12bc3f9f1445a5450db9a28d26
Vincent van Hees <v.vanhees@accelting.com>
2021-12-08 10:34:58 +0100

    inside R change working directory to data folder, because that is where the mounted volume is

--- Docker/code/app.R
+++ Docker/code/app.R
@@ -1,9 +1,8 @@
 library("HabitusGUI")
-# if ("data" %in% dir()) {
-#   homedir = paste0(getwd(),"/data") # cd to data directory
-#   setwd(homedir)
-# } else {
-#   homedir = getwd()
-# }
-# HabitusGUI::myApp(homedir)
-HabitusGUI::myApp(h)
+if ("data" %in% dir()) {
+  homedir = paste0(getwd(),"/data") # cd to data directory
+  setwd(homedir)
+} else {
+  homedir = getwd()
+}
+HabitusGUI::myApp(homedir)
